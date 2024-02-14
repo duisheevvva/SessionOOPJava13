6ff8887b48c3a60a12e662147c45ebdf7d356691
duisheevvva <duisheeva.aizat@icloud.com>
2024-02-14 11:21:33 +0600

    task 1 session Constructor

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,31 @@
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("test");
+
+//        Bez parametr
         Student student1 = new Student();
         student1.name = "Beknazar";
-        student1.age = 16;
-        student1.group = "JS";
-
+        student1.dateOfBirth = LocalDate.of(2007,06,05);
+        student1.phoneNumber = "708665544";
+        student1.nationality = "kyrgyz";
+//        Bez parametr
         Student student2 = new Student();
         student2.name ="Sabina";
-        student2.age =  19;
-        student2.group = "JAVA";
+       student2.dateOfBirth = LocalDate.of(2004,06,05);
+       student2.phoneNumber = "706554433";
+       student2.nationality = "kyrgyz";
 
-        Student student3 =  new Student();
-        student3.name = "Sultan";
-        student3.age = 21;
-        student3.group = "JS";
-
-        Student student4 = new Student();
-        student4.name = "Nurkyz";
-        student4.age = 18;
-        student4.group = "JAVA";
-
-        Student student5 = new Student();
-        student5.name = "Joomart";
-        student5.age = 18;
-        student5.group = "JS";
+//       S parametr
+        Student student3 =  new Student("Nurkyz",LocalDate.of(2004,05,05),"70665544","kyrgyz");
+        Student student4 =  new Student("Kudaiberdi",LocalDate.of(2004,05,05),"70665544","kyrgyz");
+        Student student5 =  new Student("Yzaat",LocalDate.of(2004,05,05),"70665544","kyrgyz");
 
         Student [] students = {student1,student2,student3,student4,student5};
-
-        int countJava= 0;
-        int countJs= 0;
-        for (int i = 0; i < students.length; i++) {
-            if (students[i].group.equalsIgnoreCase("Java")){
-                countJava++;
-            }else {
-                countJs++;
-            }
+        for (Student student:students) {
+            System.out.println(student);
+            System.out.println(student.getAge());
         }
-
-        System.out.println("Count java: " + countJava);
-        System.out.println("Count js: " + countJs);
     }
 }
